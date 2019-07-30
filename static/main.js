de6c5333b90b03d17eff944f2d339ff6c5d5c447
ian-lowe <ilowe@mac.com>
2019-07-29 22:01:14 -0700

    removed footer & made effects work for all podcast episodes.

--- static/main.js
+++ static/main.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 let blur = 0;
 const clink = new Audio("/static/clink.wav");
-const audio = document.querySelector("audio");
+const audio = document.querySelectorAll("audio");
 const background = document.querySelector("body");
 const slider = document.getElementById("myRange");
 
@@ -12,11 +12,15 @@ function drunkify() {
     let input_val = document.querySelector("input[name=drink]:checked").value;
     if (slider.value < 20) {
         if (input_val === "beer" || input_val === "wine") {
-            audio.playbackRate -= 0.02;
+            for (podcast of audio) {
+                podcast.playbackRate -= 0.02;
+            }
             blur += 0.1;
             slider.value = parseInt(slider.value) + 1;
         } else {
-            audio.playbackRate -= 0.05;
+            for (podcast of audio) {
+                podcast.playbackRate -= 0.05;
+            }
             blur += 0.2;
             slider.value = parseInt(slider.value) + 2;
         }
