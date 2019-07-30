5abdabb07b8df7a4499bc4d0c099ad13e3cd380b
ian-lowe <ilowe@mac.com>
2019-07-29 18:58:54 -0700

    added variations on blur/playbackspeed/slider val depending on which beverage is selected.

--- static/main.js
+++ static/main.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector("button").onclick = drunkify;
+    document.querySelector("button").onclick = drunkify;
 });
 
-blur = 0;
-blackout = 1000;
-clink = new Audio("/static/clink.wav");
+let blur = 0;
+const clink = new Audio("/static/clink.wav");
+const audio = document.querySelector("audio");
+const background = document.querySelector("body");
+const slider = document.getElementById("myRange");
 
 function drunkify() {
-  if (document.getElementById("myRange").value < 10) {
-    audio = document.querySelector("audio");
-    background = document.querySelector("body");
-    audio.playbackRate -= 0.05;
-    blur += 0.2;
-    background.style.filter = `blur(${blur}px)`;
-    clink.play();
-    document.getElementById("myRange").value++;
-  }
+    let input_val = document.querySelector("input[name=drink]:checked").value;
+    if (slider.value < 20) {
+        if (input_val === "beer" || input_val === "wine") {
+            audio.playbackRate -= 0.02;
+            blur += 0.1;
+            slider.value = parseInt(slider.value) + 1;
+        } else {
+            audio.playbackRate -= 0.05;
+            blur += 0.2;
+            slider.value = parseInt(slider.value) + 2;
+        }
+        background.style.filter = `blur(${blur}px)`;
+        clink.play();
+    }
 }
