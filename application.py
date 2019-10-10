34148c8500179656d9bd598e8537e6b9b07f0173
ian-lowe <ilowe@mac.com>
2019-10-10 14:05:14 -0700

    fixes for deployment. added dependencies to requirements.txt

--- application.py
+++ application.py
@@ -2,33 +2,37 @@ import requests
 import os
 
 from flask import Flask, render_template, jsonify
+
 app = Flask(__name__)
 
 # Check for environment variable
 if not os.getenv("API_KEY"):
     raise RuntimeError("API_KEY is not set")
 
-@app.route('/')
+
+@app.route("/")
 def index():
 
     url = "https://listen-api.listennotes.com/api/v2/podcasts/ff9de4bef29f4153a84fceb1207daa57?sort=recent_first"
-    headers = {'X-ListenAPI-Key': os.getenv("API_KEY")}
+    headers = {"X-ListenAPI-Key": os.getenv("API_KEY")}
 
     r = requests.get(url, headers=headers)
     r_json = r.json()
 
-    return render_template('index.html', r_json=r_json)
+    return render_template("index.html", r_json=r_json)
 
-@app.route('/api')
+
+@app.route("/api")
 def api():
 
     url = "https://listen-api.listennotes.com/api/v2/podcasts/ff9de4bef29f4153a84fceb1207daa57?sort=recent_first"
-    headers = {'X-ListenAPI-Key': os.getenv("API_KEY")}
+    headers = {"X-ListenAPI-Key": os.getenv("API_KEY")}
 
     r = requests.get(url, headers=headers)
     r_json = jsonify(r.json())
 
     return r_json
+
 
 if __name__ == "__main__":
     app.run()
